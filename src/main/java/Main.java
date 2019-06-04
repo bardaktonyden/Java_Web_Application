8d035b80434a97d1bde9066cbed6980926470a67
Anton Denisenko <bardaktonyden@yandex.ru>
2019-06-04 11:03:26 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main {
-    public static void main(String args[]) throws Exception{
+    public static void main(String args[]) throws Exception {
         Frontend frontend = new Frontend();
 
         Server server = new Server(8080);
